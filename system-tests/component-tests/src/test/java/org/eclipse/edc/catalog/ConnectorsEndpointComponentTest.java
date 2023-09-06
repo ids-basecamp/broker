20cae00c4877e4e53608b9d85d0bf3887a82b040
Glaucio Jannotti <glaucio.jannotti@dengun.com>
2023-09-06 17:35:41 -0300

    fix: connector endpoint tests intermittent fails

--- system-tests/component-tests/src/test/java/org/eclipse/edc/catalog/ConnectorsEndpointComponentTest.java
+++ system-tests/component-tests/src/test/java/org/eclipse/edc/catalog/ConnectorsEndpointComponentTest.java
@@ -20,13 +20,13 @@ import org.eclipse.edc.catalog.spi.FederatedCacheNode;
 import org.eclipse.edc.catalog.spi.directory.FederatedCacheNodeDirectory;
 import org.eclipse.edc.junit.annotations.ComponentTest;
 import org.eclipse.edc.junit.extensions.EdcExtension;
+import org.eclipse.edc.spi.EdcException;
 import org.eclipse.edc.spi.message.MessageContext;
 import org.eclipse.edc.spi.message.RemoteMessageDispatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 
 import java.time.Duration;
 import java.util.List;
@@ -56,7 +56,7 @@ public class ConnectorsEndpointComponentTest {
     void setup(EdcExtension extension) {
         extension.setConfiguration(Map.of(
                 // make sure only one crawl-run is performed
-                "edc.catalog.cache.execution.period.seconds", "2",
+                "edc.catalog.cache.execution.period.seconds", "5",
                 // number of crawlers will be limited by the number of crawl-targets
                 "edc.catalog.cache.partition.num.crawlers", "10",
                 // give the runtime time to set up everything
@@ -68,7 +68,7 @@ public class ConnectorsEndpointComponentTest {
 
     @Test
     @DisplayName("Get Connectors endpoint with empty result")
-    void queryConnector_withEmptyResults(FederatedCacheNodeDirectory directory) {
+    void queryConnector_withEmptyResults() {
         assertThat(queryConnectorsApi()).hasSize(0);
     }
 
@@ -92,40 +92,47 @@ public class ConnectorsEndpointComponentTest {
     }
 
     @Test
-    @DisplayName("Get Connectors endpoint with one result and crawler execution data not null")
-    void queryConnector_withManyResultsCrawlerDataNotNull(RemoteMessageDispatcher dispatcher, FederatedCacheNodeDirectory directory) {
-        Mockito.reset(dispatcher);
+    @DisplayName("Get Connectors endpoint with one result with online connector status")
+    void queryConnector_withManyResultsOnlineConnector(RemoteMessageDispatcher dispatcher, FederatedCacheNodeDirectory directory) {
         // prepare node directory
         insertSingle(directory);
 
         when(dispatcher.send(eq(Catalog.class), isA(CatalogRequest.class), any(MessageContext.class)))
-                .thenReturn(randomCatalog(5))
+                .thenReturn(randomCatalog(10))
                 .thenReturn(emptyCatalog());
 
         await().pollDelay(ofSeconds(1))
                 .atMost(TEST_TIMEOUT)
                 .untilAsserted(() -> {
-                    List<FederatedCacheNode> nodes = queryConnectorsApi();
-                    assertThat(nodes).hasSize(1);
-                    assertThat(nodes.get(0).getOnlineStatus()).isTrue();
-                    assertThat(nodes.get(0).getContractOffersCount()).isEqualTo(5);
-                });
+                    var list = queryConnectorsApi();
+                    assertThat(list).hasSize(1);
 
+                    var connector = list.get(0);
+                    assertThat(connector.getOnlineStatus()).isTrue();
+                    assertThat(connector.getLastCrawled()).isNotNull();
+                    assertThat(connector.getContractOffersCount()).isEqualTo(10);
+                });
     }
 
     @Test
-    @DisplayName("Get Connectors endpoint with one result and crawler execution data not null")
+    @DisplayName("Get Connectors endpoint with one result with offline connector status")
     void queryConnector_withManyResultsOfflineConnector(RemoteMessageDispatcher dispatcher, FederatedCacheNodeDirectory directory) {
         // prepare node directory
         insertSingle(directory);
 
+        when(dispatcher.send(eq(Catalog.class), isA(CatalogRequest.class), any(MessageContext.class)))
+                .thenThrow(new EdcException("Error calling connector"));
+
         await().pollDelay(ofSeconds(1))
                 .atMost(TEST_TIMEOUT)
                 .untilAsserted(() -> {
-                    List<FederatedCacheNode> nodes = queryConnectorsApi();
-                    assertThat(nodes).hasSize(1);
-                    assertThat(nodes.get(0).getOnlineStatus()).isFalse();
-                });
+                    var list = queryConnectorsApi();
+                    assertThat(list).hasSize(1);
 
+                    var connector = list.get(0);
+                    assertThat(connector.getOnlineStatus()).isFalse();
+                    assertThat(connector.getLastCrawled()).isNotNull();
+                    assertThat(connector.getContractOffersCount()).isEqualTo(0);
+                });
     }
 }
